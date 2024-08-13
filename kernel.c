d753765f044a92c112e01f3dae9eec448e968412
aliai <aliaidan02@gmail.com>
2024-08-13 21:11:44 +0500

    Terminal functions added to kernel

--- kernel.c
+++ kernel.c
@@ -1,29 +1,17 @@
+#include "kernel_files/tty.h"
+
 static char* const VGA_MEMORY = (char*)0xb8000;
 static int const VGA_WIDTH = 80;
 static int const VGA_HEIGHT = 25;
 
 
 void kernel_early(void){
-
+    terminal_initialize();
 }
 
+__attribute__((noreturn))
 int main(void *){
-    const char* str = "Hello world!";
-    
-    unsigned int i = 0; // for text string
-    unsigned int j = 0; // for video memory
-
-    while(str[i] != '\0'){
-        // VGA_MEMORY[j] - character itself
-        // VGA_MEMORY[j+1] - color of character
-        // Video memory accepts two bytes for each character, first byte for character and second is color
-
-        VGA_MEMORY[j] = str[i];
-        VGA_MEMORY[j+1] = 0x07;
-
-        i++;
-        j += 2; 
-    }
-
+    printf("Hello world!\n");
+    while (1){}
     return 0;
 }
