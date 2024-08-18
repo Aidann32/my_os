343bd4aba1b50ed81fb24531c3c3c2140a5acfca
aliai <aliaidan02@gmail.com>
2024-08-18 19:32:55 +0500

    Updated main file

--- kernel.c
+++ kernel.c
@@ -1,4 +1,7 @@
 #include "kernel_files/tty.h"
+#include "kernel_files/io.h"
+#include "kernel_files/kbd.h"
+#include "libc/include/string.h"
 
 static char* const VGA_MEMORY = (char*)0xb8000;
 static int const VGA_WIDTH = 80;
@@ -11,7 +14,26 @@ void kernel_early(void){
 
 __attribute__((noreturn))
 int main(void){
-    printf("Hello world!\n");
-    while (1){}
+    char* buff;
+    strcpy(&buff[strlen(buff)], "");
+    printprompt();
+    while (1){
+        uint8_t byte;
+        while(byte=scan()) {
+            if (byte == 0x1c){
+                if (strlen(buff) > 0 && strcmp(buff, "shutdown") == 0) printf("\nShutting down");
+                printprompt();
+                memset(&buff[0], 0 ,sizeof(buff));
+                break;
+            } else{
+                char c = normalmap[byte];
+                char* s;
+                s = ctos(s, c);
+                printf("%s", s);
+                strcpy(&buff[strlen(buff)], s);
+            }
+            move_cursor(get_terminal_row(), get_terminal_col());
+        }
+    }
     return 0;
 }
